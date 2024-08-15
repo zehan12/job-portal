b08e498f3df3187617d36cdaf4584a7711e5ee49
zehan12 <zehan9211@gmail.com>
2024-08-15 13:00:00 +0530

    implement dark mode  and theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import Image from "next/image";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="text-4xl uppercase font-medium text-foreground/85">Job Portal</h1>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-background">
+            <h1 className="text-4xl uppercase font-medium text-foreground">Job Portal
+              <ThemeToggle />
+            </h1>
         </main>
     );
 }
